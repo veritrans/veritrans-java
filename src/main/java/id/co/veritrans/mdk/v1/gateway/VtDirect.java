9a1fe513d5129fcfe572cbc487bb87aef92cdfae
Gde Mahardhikha Satrigraha <gde.satrigraha@veritrans.co.id>
2015-05-13 13:43:18 +0700

    update javadoc

--- src/main/java/id/co/veritrans/mdk/v1/gateway/VtDirect.java
+++ src/main/java/id/co/veritrans/mdk/v1/gateway/VtDirect.java
@@ -13,6 +13,8 @@ public interface VtDirect extends VtGateway {
      * Send a payment charge requests to Veritrans.
      * @param vtDirectChargeRequest Veritrans charging message request. {@link id.co.veritrans.mdk.v1.gateway.model.vtdirect.CreditCardRequest Credit card request}, {@link id.co.veritrans.mdk.v1.gateway.model.vtdirect.BankTransferRequest bank transfer request}, {@link id.co.veritrans.mdk.v1.gateway.model.vtdirect.MandiriClickpayRequest mandiri clickpay request}. {@link id.co.veritrans.mdk.v1.gateway.model.vtdirect.CimbClicksRequest cimb clicks request}, or {@link id.co.veritrans.mdk.v1.gateway.model.vtdirect.BriEpayRequest bri epay request}.
      * @return {@link id.co.veritrans.mdk.v1.gateway.model.VtResponse Veritrans response}
+     * @throws {@link id.co.veritrans.mdk.v1.exception.RestClientException} when an exception was occurred during
+     * executing the request.
      */
     VtResponse charge(VtDirectChargeRequest vtDirectChargeRequest) throws RestClientException;
 
@@ -21,6 +23,8 @@ public interface VtDirect extends VtGateway {
      * @param transactionId {@link id.co.veritrans.mdk.v1.gateway.model.VtResponse#getTransactionId() Transaction id} of authorized request
      * @param amount        Total amount that will be captured (less or equal then authorized gross amount)
      * @return              {@link id.co.veritrans.mdk.v1.gateway.model.VtResponse Veritrans response}
+     * @throws {@link id.co.veritrans.mdk.v1.exception.RestClientException} when an exception was occurred during
+     * executing the request.
      */
     VtResponse capture(String transactionId, Long amount) throws RestClientException;
 }
--- src/main/java/id/co/veritrans/mdk/v1/gateway/VtGateway.java
+++ src/main/java/id/co/veritrans/mdk/v1/gateway/VtGateway.java
@@ -6,7 +6,7 @@ import id.co.veritrans.mdk.v1.gateway.model.VtResponse;
 import java.io.UnsupportedEncodingException;
 
 /**
- * VT Gateway
+ * VT Gateway.
  */
 public interface VtGateway {
 
@@ -15,6 +15,9 @@ public interface VtGateway {
      *
      * @param orderId   {@link id.co.veritrans.mdk.v1.gateway.model.VtResponse#orderId Order id} of transaction
      * @return          {@link id.co.veritrans.mdk.v1.gateway.model.VtResponse Veritrans response}
+     * @throws {@link id.co.veritrans.mdk.v1.exception.RestClientException} when an exception was occurred during
+     * executing the request.
+     * @throws UnsupportedEncodingException when UTF-8 Encoding is not available.
      */
     VtResponse approve(String orderId) throws RestClientException, UnsupportedEncodingException;
 
@@ -23,6 +26,9 @@ public interface VtGateway {
      *
      * @param orderId   {@link id.co.veritrans.mdk.v1.gateway.model.VtResponse#orderId Order id} of transaction
      * @return          {@link id.co.veritrans.mdk.v1.gateway.model.VtResponse Veritrans response}
+     * @throws {@link id.co.veritrans.mdk.v1.exception.RestClientException} when an exception was occurred during
+     * executing the request.
+     * @throws UnsupportedEncodingException when UTF-8 Encoding is not available.
      */
     VtResponse status(String orderId) throws RestClientException, UnsupportedEncodingException;
 
@@ -31,8 +37,9 @@ public interface VtGateway {
      *
      * @param orderId   {@link id.co.veritrans.mdk.v1.gateway.model.VtResponse#orderId Order id} of the transaction
      * @return          {@link id.co.veritrans.mdk.v1.gateway.model.VtResponse Veritrans response}
-     * @throws RestClientException
-     * @throws UnsupportedEncodingException
+     * @throws {@link id.co.veritrans.mdk.v1.exception.RestClientException} when an exception was occurred during
+     * executing the request.
+     * @throws UnsupportedEncodingException when UTF-8 Encoding is not available.
      */
     VtResponse cancel(String orderId) throws RestClientException, UnsupportedEncodingException;
 }
