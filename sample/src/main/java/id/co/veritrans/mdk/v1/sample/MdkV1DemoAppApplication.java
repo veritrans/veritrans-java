33b27e70bfa228d64f0afebe1404e9169864c20b
Gde Mahardhikha Satrigraha <gde.satrigraha@veritrans.co.id>
2015-05-14 15:49:05 +0700

    initial sample project integration

---
+++ sample/src/main/java/id/co/veritrans/mdk/v1/sample/MdkV1DemoAppApplication.java
@@ -0,0 +1,12 @@
+package id.co.veritrans.mdk.v1.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdkV1DemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdkV1DemoAppApplication.class, args);
+    }
+}
